063c6f8248265e9155427bdd24675adb91883844
BOBBY-NS <87419130+BOBBY-NS@users.noreply.github.com>
2022-02-21 14:17:34 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -3,7 +3,7 @@
 require "vendor/autoload.php";
 require_once('vendor/linecorp/line-bot-sdk/line-bot-sdk-tiny/LINEBotTiny.php');
 
-$access_token = '3ALKAbKFoGuJyJnoDdn0HeyfbxLFtEXBKiC0lFeoNl/XbL4WhoCZzefp2n7UDuXaCWfErIDro07BnZNggJmXJChXTIlMPo8LRJ+n1LEgbRUaKehDkiCr5p5CakHrPX+gauOGX/R5bB2e5yi7xjnHDAdB04t89/1O/w1cDnyilFU=';
+$access_token = 'H5bUh6VbEuHQ0MeDykJlXIEBM3pk/+BiZpHPLi+Kf1INXB+H5xEIgF0Slu9hUzaMZswmrV6f0VfZMqkhKXqK/ANZcJSO5PzsjySA69Qgs9usMo5eSf8wYG3ngzz/W0Z6gerGWrCVxJ2E1ZLxOs8lZQdB04t89/1O/w1cDnyilFU=';
 
 // Get POST body content
 $content = file_get_contents('php://input');
