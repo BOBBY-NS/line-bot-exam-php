29c4f177af665a46e5b75c05d6c901f7431cd722
BOBBY-NS <87419130+BOBBY-NS@users.noreply.github.com>
2022-02-21 16:20:47 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -22,8 +22,9 @@ if (!is_null($events['events'])) {
 
 			// Build message to reply back
 			$messages = [
-				'type' => 'text',
-				'text' => $text
+				'type' => 'uri',
+				'label':'https://bobbyns.com/dashboard/assets/img/logo-xl.png',
+   				'linkUri': $text,
 			];
 
 			// Make a POST Request to Messaging API to reply to sender
