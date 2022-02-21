ee486e1ca1b9c79edd98beb8dec5f055db6d2a74
BOBBY-NS <87419130+BOBBY-NS@users.noreply.github.com>
2022-02-21 17:00:14 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -21,7 +21,7 @@ if (!is_null($events['events'])) {
 			$replyToken = $event['replyToken'];
 
 			// Build message to reply back
-			$messages = [
+			$messages = [[
 				'type'=> 'flex',
 				'altText'=> 'This is a Flex Message',
 				'contents'=> [
@@ -41,7 +41,7 @@ if (!is_null($events['events'])) {
 						]
 					]
 				]
-			];
+			]];
 
 			// Make a POST Request to Messaging API to reply to sender
 			$url = 'https://api.line.me/v2/bot/message/reply';
