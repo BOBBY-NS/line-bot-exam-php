7c54c02ad4d67e471610dd444e5b17be0d5a42fc
BOBBY-NS <87419130+BOBBY-NS@users.noreply.github.com>
2022-02-23 16:38:15 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -23,7 +23,7 @@ if (!is_null($events['events'])) {
             // Build message to reply back
             $messages = [
                 'type' => 'text',
-                'text' => "คลิ๊กลิงค์เพื่อรับการแจ้งเตือน\n" . $url
+                'text' => "คลิ๊กลิงค์เพื่อรับการแจ้งเตือน (ใช้ Internet ภายใน)\n" . $url
             ];
 
             // Make a POST Request to Messaging API to reply to sender
