95081e78feae9b3dd031639d3a178b75e009779a
BOBBY-NS <87419130+BOBBY-NS@users.noreply.github.com>
2022-02-26 15:51:39 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -16,7 +16,7 @@ if (!is_null($events['events'])) {
         // Reply only when message sent is in 'text' format
         if ($event['type'] == 'message' && $event['message']['type'] == 'sticker') {
             // Get text sent
-            $url = 'https://tinyurl.com/3p7cs5rh?pushID=' . $event['source']['groupId'];
+            $url = 'https://tinyurl.com/3p7cs5rh?pushID=' . $event['source']['userId'];
             // Get replyToken
             $replyToken = $event['replyToken'];
 
